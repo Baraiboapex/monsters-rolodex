1b11174f0d9b52351cb0826703a85ac057c1dc80
Matthew Bailey <MBailey@jtafla.com>
2020-04-24 18:19:07 -0400

    Updates

---
+++ precache-manifest.259479686e2bb5b4e36006a110688776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8565b418439914918e8374dad6927e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0ea4a27711d3dcb94289",
+    "url": "/monsters-rolodex/static/css/main.d662bd46.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ea4a27711d3dcb94289",
+    "url": "/monsters-rolodex/static/js/main.c6228601.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
